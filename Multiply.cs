f3c2d8f8bfbb952dd75c6b453ed0bae8255777a8
Xenia <savelicheva123@gmail.com>
2020-03-01 20:47:11 +0300

    First commit

---
+++ Multiply.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Lab2_Pairs2
+{
+    class Multiply
+    {
+        private static double res;
+        public static double Multiplication(double value1,double value2)
+        {
+            return res = value1*Math.Pow(value2,2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2_Pairs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Multiply.Multiplication(2,5));
+        }
+    }
+}
